6d23834a49fd06b5f010dd31157294b45e67760b
Utkarshpawarpatil <pawarutkarsh3@gmail.com>
2026-09-06 20:36:15 +0530

    Refactor main.cpp by cleaning up comments and formatting

--- Assignment 3/main.cpp
+++ Assignment 3/main.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
 using namespace std;
-
-// Function to read array elements
 void read(int a[], int n)
 {
     cout << "Enter " << n << " elements: ";
-
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
     }
 }
 
-// Function to display array elements
 void display(int a[], int n)
 {
     for (int i = 0; i < n; i++)
@@ -22,18 +18,14 @@ void display(int a[], int n)
     cout << endl;
 }
 
-// Function to swap two numbers
-// Call by reference
 void swap(int &x, int &y)
 {
     int temp;
-
     temp = x;
     x = y;
     y = temp;
 }
 
-// Function to sort the array
 void sortArray(int a[], int n)
 {
     for (int i = 0; i < n - 1; i++)
@@ -48,23 +40,16 @@ void sortArray(int a[], int n)
     }
 }
 
-// Main function
 int main()
 {
     int a[50], n;
-
     cout << "Enter number of elements: ";
     cin >> n;
-
     read(a, n);
-
     cout << "\nArray before sorting: ";
     display(a, n);
-
     sortArray(a, n);
-
     cout << "Array after sorting: ";
     display(a, n);
-
     return 0;
 }
